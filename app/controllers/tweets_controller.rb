37d88237a52891d14640b065ddc5651ed4104759
GAKU <gemini.05211124@gmail.com>
2018-07-14 10:25:36 +0900

    デザイン部分の変更＋ページ遷移の変更

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -2,13 +2,18 @@ class TweetsController < ApplicationController
   def new
     @tweet=Tweet.new
   end
+
   def create
     @tweet=Tweet.new(tweet_params)
     if @tweet.save
-      redirect_to root_path
+      redirect_to @tweet
     else
       render :new
     end
+  end
+
+  def show
+    
   end
   private def tweet_params
     params.require(:tweet).permit(:text)
