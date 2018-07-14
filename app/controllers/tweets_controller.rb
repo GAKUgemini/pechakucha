d95c8557482979d62510d60ffef95634f6c91d1c
GAKU <gemini.05211124@gmail.com>
2018-07-14 09:58:10 +0900

    現段階では投稿の可否確認の為に投稿後の画面遷移の先はrootになっている。

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -3,5 +3,14 @@ class TweetsController < ApplicationController
     @tweet=Tweet.new
   end
   def create
+    @tweet=Tweet.new(tweet_params)
+    if @tweet.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+  private def tweet_params
+    params.require(:tweet).permit(:text)
   end
 end
