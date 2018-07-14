ba89e4bc8d8f9a158a6f35e52eae7780cb7044bc
GAKU <gemini.05211124@gmail.com>
2018-07-14 14:25:26 +0900

    投稿の消去、再編集機能の実装

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -19,6 +19,28 @@ class TweetsController < ApplicationController
   def index
     @tweet = Tweet.all
   end
+
+  def edit
+    @tweet = Tweet.find(params[:id])
+  end
+
+  def update
+    @tweet = Tweet.find(params[:id])
+
+    if @tweet.update(tweet_params)
+    redirect_to @tweet
+    else
+    render 'edit'
+    end
+  end
+
+  def destroy
+  @tweet = Tweet.find(params[:id])
+  @tweet.destroy
+
+  redirect_to tweet_path
+  end
+  
   private def tweet_params
     params.require(:tweet).permit(:text)
   end
