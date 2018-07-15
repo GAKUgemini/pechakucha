6feba61db328c824a00c0c1ed7092ba621f53bbb
GAKU <gemini.05211124@gmail.com>
2018-07-15 11:13:26 +0900

    login画面の生成（未完成）

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,8 @@ Rails.application.routes.draw do
   resources :tweets do
     resources :comments
   end
+  get '/login', to:'sessions#new'
+  post '/login', to:'sessions#create'
+  delete 'logout', to:'sessions#destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
