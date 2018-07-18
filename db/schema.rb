d34d442efe5e22f436175c512bdebabcd40fc2d0
GAKU <gemini.05211124@gmail.com>
2018-07-18 14:26:44 +0900

    不具合の調整

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_16_073951) do
+ActiveRecord::Schema.define(version: 2018_07_16_113857) do
 
   create_table "comments", force: :cascade do |t|
     t.text "comment"
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 2018_07_16_073951) do
     t.text "text"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
