029221d33514fa33205161c0c5e9de6cd9b50d29
GAKU <gemini.05211124@gmail.com>
2018-08-09 18:36:00 +0900

    password_confirmationの消去

--- db/migrate/20180712095612_create_users.rb
+++ db/migrate/20180712095612_create_users.rb
@@ -4,7 +4,7 @@ class CreateUsers < ActiveRecord::Migration[5.2]
       t.string :email
       t.string :username
       t.string :password
-      t.string :password_confirmation
+      #t.string :password_confirmation
       t.timestamps
     end
   end
